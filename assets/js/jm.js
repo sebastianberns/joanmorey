bef05e6ae12803862e9e64b805d5a3526fda880a
Sebastian Berns <sebastianberns@gmail.com>
2015-06-23 18:23:06 +0200

    First manifesto auto-scroll loop

--- assets/js/jm.js
+++ assets/js/jm.js
@@ -2,6 +2,61 @@ var jm = {
   
   init: function()
   {
+    jm.manifesto.init()
+  },
+  
+  manifesto: {
+    
+    m: document.getElementById('manifesto'),
+    a: document.querySelectorAll('#manifesto article'),
+    o: {},
+    s: false,
+    
+    init: function()
+    {
+      for( var i = 0; i < jm.manifesto.a.length; i++ )
+        jm.manifesto.o[ jm.manifesto.a[i].id ] = eina.offset( jm.manifesto.a[i] )
+      
+      jm.manifesto.play()
+    },
+    
+    get: {
+      
+      first: function()
+      {
+        return jm.manifesto.a[ 0 ]
+      },
+      
+      curr: function()
+      {
+        var h = parseInt( eina.vp().h / 2 )
+        for( var i = 0; i < jm.manifesto.a.length; i++ ){
+          if( jm.manifesto.o[ jm.manifesto.a[i].id ].t + h >= window.scrollY ) 
+            return jm.manifesto.a[i]
+        }
+        return null
+      },
+      
+      next: function()
+      {
+        var c = jm.manifesto.get.curr()
+        return c.nextElementSibling || jm.manifesto.get.first()
+      }
+      
+    },
+    
+    play: function()
+    {
+      jm.manifesto.s = setInterval(function(){
+        var n = '#'+ jm.manifesto.get.next().id
+        smoothScroll.animateScroll( null, n, { speed: 200, easing: 'easeOutCubic' })
+      }, 2000)
+    },
+    
+    pause: function()
+    {
+      clearInterval( jm.manifesto.s )
+    }
     
   }
   
