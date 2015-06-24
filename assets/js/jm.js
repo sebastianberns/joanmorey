2e2e553bf30164434059ec1e8447b1ea14f1b27b
Sebastian Berns <sebastianberns@gmail.com>
2015-06-25 00:09:57 +0200

    Reorganized

--- assets/js/jm.js
+++ assets/js/jm.js
@@ -1,93 +1,84 @@
 var jm = {
   
+  config: {
+    next: 2000,
+    timeout: 10000,
+    scroll: {
+      speed: 150,
+      easing: 'easeOutCubic',
+      updateURL: false
+    }
+  },
+  m: document.getElementById('manifesto'),
+  a: document.querySelectorAll('#manifesto article'),
+  o: {},
+  s: false,
+  
   init: function()
   {
-    jm.manifesto.init()
+    jm.setup()
+    // 
+    var resizeDebounce = eina.debounce( jm.setup, jm.config.next )
+    bean.on( window, 'resize', resizeDebounce )
+    
+    // Pause on mousewheel
+    var mousewheelThrottle = eina.throttle( jm.pause, 300, { trailing: false })
+    bean.on( jm.m, 'DOMMouseScroll', mousewheelThrottle, false ) // Firefox
+    bean.on( jm.m, 'mousewheel', mousewheelThrottle, false )     // all others
+    bean.on( jm.m, 'touchmove', jm.pause, false )      // Touch
+    // Resume after timeout
+    var scrollTimeout = eina.debounce( jm.play, jm.config.next )
+    bean.on( jm.m, 'DOMMouseScroll', scrollTimeout, false )      // Firefox
+    bean.on( jm.m, 'mousewheel', scrollTimeout, false )          // all others
+    bean.on( jm.m, 'touchmove', scrollTimeout, false )           // Touch  
   },
   
-  manifesto: {
+  setup: function()
+  {
+    jm.pause()
     
-    config: {
-      next: 2000,
-      timeout: 10000,
-      scroll: {
-        speed: 150,
-        easing: 'easeOutCubic',
-        updateURL: false
+    for( var i = 0; i < jm.a.length; i++ )
+      jm.o[ jm.a[i].id ] = eina.offset( jm.a[i] )
+    
+    jm.play()
+  },
+  
+  get: {
+    
+    first: function()
+    {
+      return jm.a[ 0 ]
+    },
+    
+    curr: function()
+    {
+      var h = parseInt( eina.vp().h / 2 )
+      for( var i = 0; i < jm.a.length; i++ ){
+        if( jm.o[ jm.a[i].id ].t + h >= jm.m.scrollTop ) 
+          return jm.a[i]
       }
+      return null
     },
-    m: document.getElementById('manifesto'),
-    a: document.querySelectorAll('#manifesto article'),
-    o: {},
-    s: false,
     
-    init: function()
+    next: function()
     {
-      jm.manifesto.setup()
-      // 
-      var resizeDebounce = eina.debounce( jm.manifesto.setup, jm.manifesto.config.next )
-      bean.on( window, 'resize', resizeDebounce )
-      
-      // Pause on mousewheel
-      var mousewheelThrottle = eina.throttle( jm.manifesto.pause, 300, { trailing: false })
-      bean.on( jm.manifesto.m, 'DOMMouseScroll', mousewheelThrottle, false ) // Firefox
-      bean.on( jm.manifesto.m, 'mousewheel', mousewheelThrottle, false )     // all others
-      bean.on( jm.manifesto.m, 'touchmove', jm.manifesto.pause, false )      // Touch
-      // Resume after timeout
-      var scrollTimeout = eina.debounce( jm.manifesto.play, jm.manifesto.config.next )
-      bean.on( jm.manifesto.m, 'DOMMouseScroll', scrollTimeout, false )      // Firefox
-      bean.on( jm.manifesto.m, 'mousewheel', scrollTimeout, false )          // all others
-      bean.on( jm.manifesto.m, 'touchmove', scrollTimeout, false )           // Touch  
-    },
-    
-    setup: function()
-    {
-      jm.manifesto.pause()
-      
-      for( var i = 0; i < jm.manifesto.a.length; i++ )
-        jm.manifesto.o[ jm.manifesto.a[i].id ] = eina.offset( jm.manifesto.a[i] )
-      
-      jm.manifesto.play()
-    },
-    
-    get: {
-      
-      first: function()
-      {
-        return jm.manifesto.a[ 0 ]
-      },
-      
-      curr: function()
-      {
-        var h = parseInt( eina.vp().h / 2 )
-        for( var i = 0; i < jm.manifesto.a.length; i++ ){
-          if( jm.manifesto.o[ jm.manifesto.a[i].id ].t + h >= jm.manifesto.m.scrollTop ) 
-            return jm.manifesto.a[i]
-        }
-        return null
-      },
-      
-      next: function()
-      {
-        var c = jm.manifesto.get.curr() || jm.manifesto.get.first()
-        return c.nextElementSibling || jm.manifesto.get.first()
-      }
-      
-    },
-    
-    play: function()
-    {
-      jm.manifesto.s = setInterval(function(){
-        var n = '#'+ jm.manifesto.get.next().id
-//        smoothScroll.animateScroll( null, n, jm.manifesto.config.scroll )
-      }, jm.manifesto.config.next)
-    },
-    
-    pause: function()
-    {
-      clearInterval( jm.manifesto.s )
+      var c = jm.get.curr() || jm.get.first()
+      return c.nextElementSibling || jm.get.first()
     }
     
+  },
+  
+  play: function()
+  {
+    jm.s = setInterval(function(){
+      var n = '#'+ jm.get.next().id
+//        smoothScroll.animateScroll( null, n, jm.config.scroll )
+    }, jm.config.next)
+  },
+  
+  pause: function()
+  {
+    clearInterval( jm.s )
   }
   
 }
