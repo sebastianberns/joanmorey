22b5e31d6aec569490aef218cca481ee35f4e99a
Sebastian Berns <sebastianberns@gmail.com>
2016-02-08 12:23:54 +0100

    Compass config

---
+++ config.rb
@@ -0,0 +1,9 @@
+
+css_dir = "assets/css" # by Compass.app 
+sass_dir = "assets/scss" # by Compass.app 
+images_dir = "assets/i" # by Compass.app 
+output_style = :compressed # by Compass.app 
+relative_assets = false # by Compass.app 
+line_comments = false # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
+sourcemap = false # by Compass.app 
